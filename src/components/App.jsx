0b28031f0e5d9dd7589548d799354b3170c247ac
rishabpurba <96917549+rishabpurba@users.noreply.github.com>
2023-09-10 11:27:40 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import emojipedia from "../emojipedia";
+import Term from "./term";
+function createTerm(emojis) {
+  return (
+    <Term
+      key={emojis.id}
+      emoji={emojis.emoji}
+      name={emojis.name}
+      meaning={emojis.meaning}
+    />
+  );
+}
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+
+      <dl className="dictionary">{emojipedia.map(createTerm)}</dl>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/prac.js
@@ -0,0 +1,6 @@
+import emojipedia from "./emojipedia";
+const newEmojipedia = emojipedia.map(function (emojiEntry) {
+  return emojiEntry.meaning.substring(0, 100);
+});
+
+console.log(newEmojipedia);
